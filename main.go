7e06f8170d3d7be76cdc900965bedcda51aa207c
Kato Yasuhiro (加藤 泰裕) <yasuhiro.kato@cct-inc.co.jp>
2019-04-05 14:11:58 +0900

    Change the output path to public directory by npm run build

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "net/http"
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("view/build/")))
+	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.ListenAndServe(":8080", nil)
 }
