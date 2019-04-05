3331d832cf96105b68d18daf5e6fa20a00dfc649
Kato Yasuhiro (加藤 泰裕) <yasuhiro.kato@cct-inc.co.jp>
2019-04-05 18:56:11 +0900

    Add open url with default browser

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yasukotelin/pandrabox
 
 go 1.12
+
+require github.com/yasukotelin/go-openurl v0.0.0-20190405095145-7bc8a30017d4
--- main.go
+++ main.go
@@ -1,8 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/yasukotelin/go-openurl"
+)
+
+const (
+	port = ":8080"
+)
+
+var (
+	exitCode = 0
+)
 
 func main() {
+	routing()
+	listen()
+	os.Exit(exitCode)
+}
+
+func getRunURL() string {
+	return fmt.Sprintf("http://localhost%s", port)
+}
+
+func routing() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":8080", nil)
+}
+
+func listen() {
+	fmt.Println("server started.")
+	fmt.Printf("listen on the localhost%s\n", port)
+	fmt.Println()
+	fmt.Println("stop server pressed Ctrl+C.")
+	if err := openurl.OpenWithBrowser(getRunURL()); err != nil {
+		fmt.Println(err)
+	}
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Println(err)
+		exitCode = 1
+		return
+	}
 }
